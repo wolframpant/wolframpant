1a7388ba68e9e3f5e712c669e560b71ffe83631b
Ellen <llnwlfsn@gmail.com>
2015-03-19 01:03:31 +0000

    config change

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -1,4 +1,5 @@
 CarrierWave.configure do |config|
+  
   config.fog_credentials = {
     provider:               'AWS',
     aws_access_key_id:      ENV['AWS_ACCESS_KEY_ID'],
@@ -6,4 +7,5 @@ CarrierWave.configure do |config|
   }
   config.fog_directory  = ENV['AWS_BUCKET']
   config.fog_public     = true
+  
 end
